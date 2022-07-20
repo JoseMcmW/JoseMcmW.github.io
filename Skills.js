313ba5f7aacd41705bfee12ac8fba5afe010930b
Jose Centeno <josemanuelcm25@gmail.com>
2022-07-20 11:01:09 -0300

    Skills Animations JavaScript

---
+++ Skills.js
@@ -0,0 +1,17 @@
+const imgBoxes = document.querySelectorAll('.skillsImg');
+const textImg = document.querySelectorAll('.invisibleText');
+
+for(let i = 0; i < 11; i++) {
+    imgBoxes[i].addEventListener('mouseover', function onMouseOver() {
+        if(textImg[i].classList.contains('invisibleText')){
+            textImg[i].classList.remove('invisibleText')
+        }
+    })
+}
+
+
+
+
+
+
+
